d81f90b440f7a7d8d198dccf2ea3ce1ff8f700a3
DevWambugu <venusdata100@gmail.com>
2024-01-05 20:53:49 +0000

    introduce a static method

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,35 @@
+class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Getter
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  displayFullPrice() {
+    const { name, code } = this._currency;
+    return `${this._amount} ${name} (${code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    const convertedPrice = amount * conversionRate;
+    return convertedPrice;
+  }
+}
+
+export default Pricing;
