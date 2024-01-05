5e2d9e680cebd7175e61ecda2513ec684a4559eb
DevWambugu <venusdata100@gmail.com>
2024-01-05 20:02:46 +0000

    alter syntax

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,24 +1,24 @@
 class Currency {
-  constructor(name, code) {
-    this._name = name;
+  constructor(code, name) {
     this._code = code;
+    this._name = name;
   }
 
   // Getter
-  get name() {
-    return this._name;
-  }
-
-  set name(newName) {
-    this._name = newName;
-  }
-
   get code() {
     return this._code;
   }
 
   set code(newCode) {
     this._code = newCode;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = newName;
   }
 
   displayFullCurrency() {
