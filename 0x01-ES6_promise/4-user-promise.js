cb4cef1a8fb0a624f0fea5e29d8dd311c1ec5466
DevWambugu <venusdata100@gmail.com>
2024-01-07 09:40:49 +0000

    write a simple promise

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,10 @@
+function signUpUser(firstName, lastName) {
+  return new Promise((resolve) => {
+    const user = {
+      firstName,
+      lastName,
+    };
+    resolve(user);
+  });
+}
+export default signUpUser;
