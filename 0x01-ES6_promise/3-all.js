c8822cb8f394c4b62980ae9f21f326437bf8ee31
DevWambugu <venusdata100@gmail.com>
2024-01-07 09:28:43 +0000

    add return statement

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,7 @@ function handleProfileSignup() {
   const photo = uploadPhoto();
   const user = createUser();
 
-  Promise.all([photo, user]).then((values) => {
+  return Promise.all([photo, user]).then((values) => {
     console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
   })
     .catch(() => { console.log('Signup system offline'); });
