893b046d6963539ee74adee217de45e5188b3e34
DevWambugu <venusdata100@gmail.com>
2024-02-27 02:15:03 +0300

    change syntax

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,13 +3,16 @@
 const http = require('http');
 // Create the server
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   // write the response with the recommneded response.
   res.end('Hello Holberton School!\n');
 });
 
 // Set the app to listen on port 1245
-app.listen(1245);
+app.listen(1245, 'localhost', () => {
+  console.log('Server running at http://0.0.0.0:1245/');
+});
 // Export the app variable
 
 module.exports = app;
